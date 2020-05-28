140f84e02ff615bf845ce47724fb5d0f1d26ff94
root <maria.revelo@pucese.edu.ec>
2020-05-28 19:53:10 +0000

    ingrese_datos_canciones

--- respaldoBDspotify1.sql
+++ respaldoBDspotify1.sql
@@ -113,7 +113,7 @@ CREATE TABLE `Canciones` (
   CONSTRAINT `Canciones_ibfk_1` FOREIGN KEY (`Artista_id_artista`) REFERENCES `Artista` (`id_artista`),
   CONSTRAINT `Canciones_ibfk_2` FOREIGN KEY (`Album_id_album`) REFERENCES `Album` (`id_album`),
   CONSTRAINT `Canciones_ibfk_3` FOREIGN KEY (`Genero_id_genero`) REFERENCES `Genero` (`id_genero`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,6 +122,7 @@ CREATE TABLE `Canciones` (
 
 LOCK TABLES `Canciones` WRITE;
 /*!40000 ALTER TABLE `Canciones` DISABLE KEYS */;
+INSERT INTO `Canciones` VALUES (2,'Plato',2,2,2),(3,'Madison',3,3,3),(4,'Irene',4,4,4),(5,'Sandra',5,5,5),(6,'Veronica',6,6,6),(7,'Kelsie',7,7,7),(8,'Leroy',8,8,8),(9,'Len',9,9,9),(10,'April',10,10,10),(11,'Macy',11,11,11),(12,'Sarah',12,12,12),(13,'Hammett',13,13,13),(14,'Gareth',14,14,14),(15,'Neve',15,15,15),(16,'Dylan',16,16,16),(17,'Lester',17,17,17),(18,'Kennan',18,18,18),(19,'Marny',19,19,19),(20,'Patience',20,20,20),(21,'Fritz',21,21,21),(22,'Uriel',22,22,22),(23,'Cairo',23,23,23),(24,'Rosalyn',24,24,24),(25,'Patrick',25,25,25),(26,'Flynn',26,26,26),(27,'Xerxes',27,27,27),(28,'Lesley',28,28,28),(29,'Forrest',29,29,29),(30,'Constance',30,30,30),(31,'Aspen',31,31,31),(32,'Jocelyn',32,32,32),(33,'Giacomo',33,33,33),(34,'Kieran',34,34,34),(35,'Lillith',35,35,35),(36,'Gretchen',36,36,36),(37,'Willa',37,37,37),(38,'Hop',38,38,38),(39,'Haviva',39,39,39),(40,'Brendan',40,40,40),(41,'Amanda',41,41,41),(42,'Zephania',42,42,42),(43,'Arden',43,43,43),(44,'Beverly',44,44,44),(45,'Chloe',45,45,45),(46,'Wyatt',46,46,46),(47,'Quincy',47,47,47),(48,'Wanda',48,48,48),(49,'Aileen',49,49,49),(50,'Upton',50,50,50),(51,'Hayley',51,51,51),(52,'Harriet',52,52,52),(53,'Noelani',53,53,53),(54,'Kato',54,54,54),(55,'Barclay',55,55,55),(56,'Martena',56,56,56),(57,'Brody',57,57,57),(58,'Nicole',58,58,58),(59,'Reed',59,59,59),(60,'Griffith',60,60,60),(61,'Lawrence',61,61,61),(62,'Camille',62,62,62),(63,'Amir',63,63,63),(64,'Otto',64,64,64),(65,'Jack',65,65,65),(66,'Patricia',66,66,66),(67,'Carter',67,67,67),(68,'Shellie',68,68,68),(69,'Noah',69,69,69),(70,'Hoyt',70,70,70),(71,'Flynn',71,71,71),(72,'Reece',72,72,72),(73,'Louis',73,73,73),(74,'Martha',74,74,74),(75,'Michelle',75,75,75),(76,'Cameron',76,76,76),(77,'Roary',77,77,77),(78,'Amos',78,78,78),(79,'Ray',79,79,79),(80,'Carly',80,80,80),(81,'Ciaran',81,81,81),(82,'Felix',82,82,82),(83,'Britanni',83,83,83),(84,'Quinn',84,84,84),(85,'Yvonne',85,85,85),(86,'Judah',86,86,86),(87,'Omar',87,87,87),(88,'Xyla',88,88,88),(89,'Benedict',89,89,89),(90,'Shannon',90,90,90),(91,'Aladdin',91,91,91);
 /*!40000 ALTER TABLE `Canciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -463,4 +464,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-28 19:49:56
+-- Dump completed on 2020-05-28 19:52:37
